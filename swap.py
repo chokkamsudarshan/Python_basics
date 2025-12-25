95a77ff400fd233d262f125a789499e8dc65e417
CHOKKAMSUDARSHAN <CHOKKAMSUDARSHAN@GMAIL.COM>
2025-12-25 10:57:20 +0530

    first commit onit

---
+++ swap.py
@@ -0,0 +1,8 @@
+a= input()
+b=input()
+print("original values of a={}".format(a))
+print("original value of b={}".format(b))
+#swap
+a,b=b,a
+print("swapped values of a={}".format(a))
+print("swapped value of b={}".format(b))
